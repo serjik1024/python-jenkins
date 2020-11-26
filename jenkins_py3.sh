6e30768b551e7e3877b726eebe9a0075963d884c
serjik1024 <72664251+serjik1024@users.noreply.github.com>
2020-11-25 19:17:31 -0800

    Create jenkins_py3.sh

---
+++ jenkins_py3.sh
@@ -0,0 +1,18 @@
+sudo apt update -y
+sudo apt install python3-pip python3-dev -y
+
+# Next line is for command line test
+# PYENV_HOME="/home/labsuser/jenkins/workspace/python-freestyle/.sample1"
+PYENV_HOME="$WORKSPACE/.sample1"
+
+if [[ -d $PYENV_HOME ]]; then
+	rm –rf $PYENV_HOME
+fi
+
+python3 -m venv $PYENV_HOME
+chmod +x $PYENV_HOME/bin/activate
+$PYENV_HOME/bin/activate
+
+pip install -r ./requirements.txt
+
+flask run
